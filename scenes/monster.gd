22c192004a296e9a4568abfa0a44498d41790160
Suspicious-Garbage <70545246+Suspicious-Garbage@users.noreply.github.com>
2026-08-30 22:41:43 -0300

    una ia minima

--- scenes/monster.gd
+++ scenes/monster.gd
@@ -5,29 +5,75 @@ signal died(id)
 	
 @onready var _detection_range = $DetectionRange
 
-var is_dead = false
+var _state = 0
+var _direction: Vector2
+var _player: Node
+var _in_range: bool = false
+var _counter: int = 0
+var _shots = []
 
 func _process(delta: float) -> void:
 	pass
 	
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_text_backspace"):
-		fired.emit(position, Vector2(-200,0))
+func _ready():
+	_state_transition()
 
-
-func _on_timer_timeout() -> void:
+func _state_transition() -> void:
+	_in_range = false
 	var _targets = _detection_range.get_overlapping_bodies()
+	
+	
+	if _targets.size() != 0:
+		_player = _targets[0]
+		print(_player)
+		_in_range = true
+	
+		match _state:
+			0: 
+				_direction = (_targets[0].position - position).normalized()
+				$ShootTimer.start(0.1)
+				_shots = [
+					[_direction, position],
+					[_direction, position],
+					[_direction, position],
+					[_direction, position],
+					[_direction, position]
+				]
+				_state = 1
+			1:
+				_state = 0	
+	$StateTimer.start(1)
+	
+#func _physics_process(delta: float) -> void:
 
-	if _targets.size() == 0:
-		return
-	
-	var _player = _targets[0]
-	
-	var _direction = (_player.position - position).normalized()
-	
-	fired.emit(position, _direction * 200)
+func _on_shoot_timer():
+	if _shots.size() > 0:
+		var params = _shots.pop_front()
+		fired.emit(params[1], params[0] * 200)
+	else:
+		$ShootTimer.stop()
+		state_transition()
+
+func _on_timer_2_timeout() -> void:
+	match _state:
+		1:
+			_counter += 1
+			
+			if _counter >= 6:
+				_state = 0
+			else:
+				$Timer2.start(3)
+				fired.emit(position, _direction * 200)
 
 
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	died.emit(self)
+	
+
+
+
+
+
+func state_transition() -> void:
+	pass # Replace with function body.
